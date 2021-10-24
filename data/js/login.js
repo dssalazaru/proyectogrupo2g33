311e899db3823731963c3e7a5859ecc537c1c767
dssalazaru <dssalazaru@gmail.com>
2021-10-24 15:27:04 -0500

    Update data

--- data/js/login.js
+++ data/js/login.js
@@ -1,22 +1,22 @@
 function ir (){
 
-  var c="2468";
-  var u="NAME";
+  var c="admin";
+  var u="admin";
   
-  var correo = document.getElementById("inputmail").value
+  var usuario = document.getElementById("inputmail").value
   
   var nill="";
   var contra = document.getElementById("inputpassword").value
   
-      if (contra==c & correo==u){
+      if (contra==c & usuario==u){
       alert("Bienvenido a G2 Inventory");
       window.location="inventory.html";}
         else if (contra==nill){
         alert("No ingreso contraseña intentelo de nuevo.");}
-        else if (correo==nill){
-          alert("No ingreso un correo intentelo de nuevo.");}
+        else if (usuario==nill){
+          alert("No ingreso un usuario intentelo de nuevo.");}
           else if (contra==nill & contra==nill){
-            alert("No ingreso contraseña ni correo intentelo de nuevo.");}
+            alert("No ingreso contraseña ni usuario intentelo de nuevo.");}
            else{ 
            alert("Porfavor ingrese, nombre y usuario correcto.");
         
--- src/app/components/inventario/inventario.component.ts
+++ src/app/components/inventario/inventario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-inventario',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventarioComponent implements OnInit {
 
-  constructor() { }
+  totalAngularPackages:any;
 
-  ngOnInit(): void {
-  }
+  constructor(private http: HttpClient) { }
+
+  token:String = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTYzNTEwMjk4OCwiZXhwIjoxNjM1MTA0MTg4LCJ1c2VybmFtZSI6ImFkbWluIiwiaWQiOjF9.cmw9uWYXtdkk1-DAXxTQZI1dNuSPZE-ZT2M9DQHwuqo";
+ 
+  ngOnInit() {
+    const headers = { 'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTYzNTEwNDYzNCwiZXhwIjoxNjM1MTA1ODM0LCJ1c2VybmFtZSI6ImFkbWluIiwiaWQiOjF9.KmnM-DBQM-h-fm--xlaj3uHd-iJCz_61aSDjnZzNW9E', 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'localhost' }
+    this.http.get<any>('http://app.dssu.xyz:8080/g2inventory/api/inventario', { headers }).subscribe(data => {
+        this.totalAngularPackages = data.tsotal;
+    })
+    // console.log(this.totalAngularPackages)
+}
+
+
+
 
 }
