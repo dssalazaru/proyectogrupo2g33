0f3b1b13e226fbe6c17f67e4f81b897023152574
dssalazaru <dssalazaru@gmail.com>
2021-10-24 11:29:29 -0500

    Carga de archivos inventario angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InventarioComponent } from './components/inventario/inventario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventarioComponent
   ],
   imports: [
     BrowserModule
